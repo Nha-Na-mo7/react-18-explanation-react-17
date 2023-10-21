e81063b54f7c3a6649db89b367a2c61aef77bfd7
Nha-Na-mo7 <zirconiump42@gmail.com>
2023-10-21 18:21:09 +0900

    feat: delete useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,7 @@
-import React, { useEffect } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  useEffect(() => {
-    console.log("useEffect App が レンダリングされました。(@v17)");
-  }, []);
-
   return (
     <div className="App">
       <header className="App-header">
